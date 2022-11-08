40e9d1e44a330502f28aa2f8a30f07217fe44555
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-11-09 02:02:46 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,8 @@ const syncServer = () => {
   gulp.watch('source/video/**', gulp.series(copy, refresh));
   gulp.watch('source/downloads/**', gulp.series(copy, refresh));
   gulp.watch('source/*.php', gulp.series(copy, refresh));
+  gulp.watch('source/favicon.ico', gulp.series(copy, refresh));
+  gulp.watch('source/manifest.webmanifest', gulp.series(copy, refresh));
 };
 
 const refresh = (done) => {
--- source/js/modules/accordions/accordion.js
+++ source/js/modules/accordions/accordion.js
@@ -2,6 +2,8 @@ const links = document.querySelector('.footer__links-container');
 const contacts = document.querySelector('.footer__contacts-container');
 const linksButton = document.querySelector('[data-open-links]');
 const contactsButton = document.querySelector('[data-open-contacts]');
+const paragraph = document.querySelector('#paragraph');
+const detailsButton = document.querySelector('[data-open-full]');
 
 const initLinks = () => {
   links.classList.toggle('is-open');
@@ -11,7 +13,17 @@ const initContacts = () => {
   contacts.classList.toggle('is-open');
 };
 
+const initDetails = () => {
+  if (detailsButton.textContent === 'ПОДРОБНЕЕ') {
+    detailsButton.textContent = 'СВЕРНУТЬ';
+  } else {
+    detailsButton.textContent = 'ПОДРОБНЕЕ';
+  }
+  paragraph.classList.toggle('is-open');
+};
+
 linksButton.addEventListener('click', initLinks);
 contactsButton.addEventListener('click', initContacts);
+detailsButton.addEventListener('click', initDetails);
 
 export {initLinks, initContacts};
